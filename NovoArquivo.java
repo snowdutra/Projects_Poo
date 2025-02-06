6322bade600bc3df766c84a7200d2a99023a4ddf
Gustavo Dutra <gusdutratelles@icloud.com>
2025-02-06 17:02:26 -0300

    Adiciona NovoArquivo.java

---
+++ NovoArquivo.java
@@ -0,0 +1,6 @@
+public class NovoArquivo {
+
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+    }
+}
---
+++ src/main/java/poo/project_01/Helo.java
@@ -0,0 +1,9 @@
+package poo.projeto_01;
+
+public class Helo {
+
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!");
+    }
+    
+}
