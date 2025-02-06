850d29710f05ba131e93763edf6f2951adacef86
Gustavo Dutra <159259903+snowdutra@users.noreply.github.com>
2025-02-06 10:08:49 -0300

    Add files via upload

---
+++ projects_poo/src/main/java/poo/aula01/TabuadaFor.java
@@ -0,0 +1,12 @@
+package poo.aula01;
+
+public class TabuadaFor {
+    
+    public static void main(String[] args) {
+        int n = 7;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }        
+    }
+
+}
