618978a2df53f3798397615cf5658d1e21e91074
Gustavo Dutra <159259903+snowdutra@users.noreply.github.com>
2025-02-11 13:21:16 +0000

    Fatorial

---
+++ src/main/java/poo/project_02/Fatorial.java
@@ -0,0 +1,12 @@
+package poo.project_02;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        int n = 5;
+        int f = 1;
+        for (int i = 1; i <= n; i++) {
+            f = f * i;
+        }
+        System.out.println(n + "! = " + f);
+    }
+}
