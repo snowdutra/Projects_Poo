d679fa26d3be779afa471c0d5e5e3fb8123f7dc1
Gustavo Dutra <159259903+snowdutra@users.noreply.github.com>
2025-02-25 13:32:48 +0000

    executavel

---
+++ src/main/java/poo/project_04/App.java
@@ -0,0 +1,13 @@
+package main.java.poo.project_04;
+
+public class App {
+    public static void main(String[] args) {
+
+        // uso de um metodo estatico, onde 
+        // noae eh necessario instanciar um objeto,
+        // pois o metodo eh da classe e nao possui
+        // atributos
+        Genero genero = Genero.registrar("Drama");
+
+    }
+}
