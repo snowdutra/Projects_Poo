f64d30dda6119e01eb78fc20535221aea7dce2e6
Gustavo Dutra <159259903+snowdutra@users.noreply.github.com>
2025-02-25 13:53:51 +0000

    ator

---
+++ src/main/java/poo/project_04_cinema/Ator.java
@@ -0,0 +1,16 @@
+package poo.project_04_cinema;  
+
+
+public class Ator{
+    private String nome;
+    private String dtnascimento;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public Date getDtnascimento(){
+        return dtnascimento;
+    }
+
+}
