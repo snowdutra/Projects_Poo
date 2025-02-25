31cf815d48916f038bde36c5a766c57ce8d9868b
Gustavo Dutra <159259903+snowdutra@users.noreply.github.com>
2025-02-25 13:54:21 +0000

    genero

---
+++ src/main/java/poo/project_04_cinema/Genero.java
@@ -0,0 +1,20 @@
+package poo.project_04_cinema;
+
+public class Genero {
+    
+    private String nome;
+
+    public Genero(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+    
+    public static Genero registrar(String nome)
+    {
+        return new Genero(nome);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
