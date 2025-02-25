5efb52e6fc0d498469e6292e67b85a05d1496109
Gustavo Dutra <159259903+snowdutra@users.noreply.github.com>
2025-02-25 13:20:20 +0000

    fatorial funcional

--- src/main/java/poo/project_02/FatorialFuncional.java
+++ src/main/java/poo/project_02/FatorialFuncional.java
@@ -4,18 +4,19 @@ public class FatorialFuncional {
     public static void main(String[] args) {
         // Linguagem declarativa - funcional
 
-        // Operador ternario 
-        int n = args.length == 0
-                ? 0
-                : Integer.parseInt(args[0]);
-        long f = 1;
-        for (int c = n; c > 1; c--) {
-            f *= c;
+        // Operador ternário com tratamento de exceção
+        int n = 0;
+        try {
+            n = args.length == 0 ? 0 : Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.err.println("Argumento inválido. Por favor, insira um número inteiro.");
+            System.exit(1);
         }
+
         System.out.println(n + "! = " + fatorial(n));
     }
 
-    public static long fatorial(long n) {   
+    public static long fatorial(long n) {
         return n == 0 ? 1 : n * fatorial(n - 1);
     }
 }
