e6b6736c9d6e628ce30bfbe3960c54c006392dfe
Gustavo Dutra <159259903+snowdutra@users.noreply.github.com>
2025-02-25 13:54:09 +0000

    singleton

---
+++ src/main/java/poo/project_04_cinema/ExemploSingleton.java
@@ -0,0 +1,8 @@
+package poo.project_04_cinema;
+
+public class ExemploSingleton {
+
+    Cinema cinema1 = Cinema.getInstance();
+    Cinema cinema2 = Cinema.getInstance();
+    // cinema1 e cinema2 são o mesmo objeto
+}
