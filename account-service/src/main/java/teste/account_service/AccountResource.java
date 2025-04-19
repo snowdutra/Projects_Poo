31e51dde8c911d4279077bac5b438e0f1c73685d
Gustavo <202369987+ghostespm@users.noreply.github.com>
2025-04-18 23:13:30 -0300

    spring boot

--- account-service/src/main/java/teste/account_service/AccountResource.java
+++ account-service/src/main/java/teste/account_service/AccountResource.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AccountResource {
     
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String hello() {
-        return "hello!";
+        return "Hello World!";
     }
 
 }
--- account-service/src/main/java/teste/account_service/Application.java
+++ account-service/src/main/java/teste/account_service/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccountServiceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AccountServiceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
